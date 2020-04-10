af709e26486c26474361161265fad835679951c8
karenheyn <karenheyn@gmail.com>
2020-04-09 23:27:23 -0400

    fixed search

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const key = "9WFravDWlCEXEUO64lPcQxTZ3ZvHE6om_bGiD4b8phc";
 const secret = "Qk_ChPr6IyqESWW4du7AcMwSZOq78cWNO1O5nvThz5E";
-const URL = `https://api.unsplash.com/search/photos?page=1&per_page=30&client_id=${key}`;
+const URL = `https://api.unsplash.com/search/photos?page=1&per_page=50&client_id=${key}`;
 const input = document.querySelector(".input");
 const form = document.querySelector(".search-form");
 const background = document.querySelector(".background");
 const overlay = document.querySelector(".overlay");
+const header = document.querySelector(".title");
 let results = [];
 
 search = searchTerm => {
@@ -12,18 +13,41 @@ search = searchTerm => {
   return fetch(url)
     .then(response => response.json())
     .then(result => {
-      let resultArray = [];
       toggleStyles();
+      header.appendChild(form);
+      // header.classList.add("header-with-images");
       result.results.forEach(image => {
         console.log("shit");
         const galleryItem = document.createElement("div");
         galleryItem.className = "gallery-item";
         const imageDiv = document.createElement("div");
         imageDiv.className = "image-div";
-
         document.querySelector(".results-page").appendChild(galleryItem);
         galleryItem.appendChild(imageDiv);
-        imageDiv.innerHTML = "<img class='image' src=" + image.urls.small + ">";
+        imageDiv.innerHTML =
+          "<img class='image' src=" + image.urls.regular + ">";
+        form.classList.remove("toggle-show");
+        // document.querySelector(".results-page").prepend(form);
+        // form.classList.add("in-header");
+        input.classList.add("header-expanded");
+        form.addEventListener("submit", e => {
+          e.preventDefault();
+          // galleryItem.remove();
+          document.querySelector(".results-page").remove();
+        });
+        // galleryItem.addEventListener("click", e => {
+        //   e.preventDefault();
+        //   if (galleryItem.className != "w2") {
+        //     galleryItem.classList.add("w2");
+        //   }
+        //   if (galleryItem.className.includes("w2")) {
+        //     galleryItem.classList.toggle("w2");
+        //   }
+        // });
+        // galleryItem.addEventListener("mouseleave", e => {
+        //   e.preventDefault();
+        //   galleryItem.classList.remove("w2");
+        // });
       });
 
       console.log(result.results);
@@ -32,19 +56,13 @@ search = searchTerm => {
 };
 
 toggleStyles = () => {
+  // const searchDiv = document.createElement("div");
+  // searchDiv.className = "search-div";
   const resultsContainer = document.createElement("div");
-  // const poop = document.createElement("div");
   resultsContainer.className = "results-page";
+  // document.body.appendChild(searchDiv);
   document.body.appendChild(resultsContainer);
   console.log(results);
-};
-
-showResults = () => {
-  results.forEach(image => {
-    const imageDiv = document.createElement("div");
-    imageDiv.innerHTML = "hello";
-    document.getElementByClassName("result-page").appendChild(imageDiv);
-  });
 };
 
 input.addEventListener("focus", e => {
